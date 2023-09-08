e48329727364754b601f180da21963ea6bdd7a91
Rashid Shamloo <rashidshamloo@gmail.com>
2023-09-08 17:27:50 +0330

    Updates

--- assets/preview-94ee1461.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-7648533c.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-eb7d4cac.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-eb7d4cac.js","./iframe-7648533c.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./_getPrototype-7bdd32d6.js","./assert-a1982797.js","./index-d475d2ea.js","./_commonjs-dynamic-modules-302442b1.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-94ee1461.js.map
